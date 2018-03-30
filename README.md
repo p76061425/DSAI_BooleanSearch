30dcb6b1cafa4b725574d738289326b01b1f82ae
shengxuan <p76061425@gmil.com>
2018-03-30 12:02:48 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 nbviewer url:
+https://nbviewer.jupyter.org/github/p76061425/DSAI_HW2/blob/master/main.ipynb
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+%%time
 import pandas as pd
-
+          
 if __name__ == '__main__':
     # You should not modify this part.
     import argparse
@@ -28,112 +29,85 @@ if __name__ == '__main__':
   
     # TODO output result
     
-    source = pd.read_csv(args.source,names = ["title"])
-    query = pd.read_csv(args.query,header = None)
+    source = pd.read_csv(args.source,names = ["index","title"]).values
 
-#     print(source)
-#     print(query)
+    with open (args.query,"r",encoding = "UTF - 8") as q_f:
+        queries = q_f.readlines()
+        index_list = []
     
-    with open(args.output, 'w') as output_file:
-        a = 87
-    
-    for row in range(query.shape[0]):
-        query_list = []
-        print(query[0][row])
-        and_pos = -1
-        or_pos = -1
-        not_pos =-1
-        and_pos = query[0][row].find(" and ")
-        or_pos = query[0][row].find(" or ")
-        not_pos = query[0][row].find(" not ")
+        for q_line in queries:
+            query = q_line.split()
+            operator = query[1]
+            operands = query[::2]
 
-        if(and_pos>=0):
-            query_list = query[0][row].split(" and ")
-            print("operation:", "and")
-        elif(or_pos>=0):
-            query_list = query[0][row].split(" or ")
-            print("operation:","or")
-        elif(not_pos>=0):
-            query_list = query[0][row].split(" not ")
-            print("operation:","not")
+#             print(operator)
+#             print(operands)
 
-        print("query_list",query_list)
-        print("and_pos",and_pos)
-        print("or_pos",or_pos)
-        print("not_pos",not_pos)
-        
-        
-        print("-------------------------搜尋結果--------------------------------")    
-        
-        index_list = []        
+            if(operator == "and"):
+                and_result=[]
+                #搜尋source
+                for title in source:
+                    find_and = True
+                    #搜尋title的每個operand
+                    for op in operands:
+                        find = title[1].find(op)
+                        if(find==-1):
+                            find_and = False
+                            break
+                    if(find_and):
+                        and_result.append( str(title[0]) )
 
-        for i in range(source.shape[0]):
-
-            if(and_pos>=0):
-                find_num = 0
-                for sub_str in range (len(query_list)):                    
-                    find = source["title"][i+1].find(query_list[sub_str])
-                    if(find>=0):
-                        find_num+=1
-                        
-                if(find_num == len(query_list)):
-                    print("index:",i+1,"find:",find,source["title"][i+1])
-                    index_list.append(i+1)
-#                     print("index_lsit",index_list)
-#                     index_list_str = str(index_list).strip('[,\',]')
-#                     print(index_list_str) 
- 
-            elif(or_pos>=0):
-                find_num = 0
-                for sub_str in range (len(query_list)):
-                    if(find_num == 0):
-                        find = source["title"][i+1].find(query_list[sub_str])
+                if(len(and_result)==0):
+                    and_result.append("0")
+                
+                and_result_str = ','.join(and_result)
+                index_list.append(and_result_str)
+            
+            
+            elif(operator == "or"):
+                or_result = []
+                for title in source:
+                    find_or = False
+                    for op in operands:
+                        find = title[1].find(op)
                         if(find>=0):
-                            find_num+=1
-                if(find_num > 0):
-                    print("index:",i+1,"find:",find,source["title"][i+1])
-                    index_list.append(i+1)
-#                     print("index_lsit",index_list)
-#                     index_list_str = str(index_list).strip('[,\',]')
-#                     print(index_list_str)
- 
-            elif(not_pos>=0):
-                find_not = 0
-                find = source["title"][i+1].find(query_list[0])
-                if(find>=0):
-                    for sub_str in range (len(query_list)-1):
-                       if(find_not == 0):
-                           find = source["title"][i+1].find(query_list[sub_str+1])
-                           if(find>=0):
-                               find_not+=1
-                    if(find_not == 0):
-                        print("index:",i+1,"find_not:",find_not,source["title"][i+1])              
-                        index_list.append(i+1)
-#                         print("index_lsit",index_list)
-#                         index_list_str = str(index_list).strip('[,\',]')
-#                         print(index_list_str)
+                            find_or = True
+                            break
+                    if(find_or):
+                        or_result.append( str(title[0]) )
 
-        print("------------------------index_list_str-------------------------")
-#         if(len(index_list)==0):
-#             with open(args.output, 'a') as output_file:
-#                 output_file.write("0"+'\n')
-#         else:
-#             index_list_str = str(index_list).strip('[,\',]')
-#             index_list_str = index_list_str.strip()
-#             print(index_list_str)
-#             with open(args.output, 'a') as output_file:
-#                 output_file.write(index_list_str+'\n')
+                if(len(or_result)==0):
+                    or_result.append("0")  
+                    
+                or_result_str = ','.join(or_result)
+                index_list.append(or_result_str)
+
+                
+            elif(operator == "not"):
+                not_result = []
+                for title in source:
+                    find_not = False
+                    find_first = title[1].find(operands[0])
+                    if(find_first>=0):
+                        for i in range(len(operands)-1):
+                            find_n_op = title[1].find(operands[i+1])
+                            if(find_n_op>=0):
+                                find_not = True
+                                break
+                        if(find_not==False):
+                            not_result.append( str(title[0]) )
+
+                if(len(not_result)==0):
+                    not_result.append("0")           
+                    
+                not_result_str = ",".join(not_result) 
+                index_list.append(not_result_str)
+
+#             print("="*80)
+            
+        index_list_str = "\n".join(index_list)
         
-        if(len(index_list)!=0):
-            n1_index = index_list[0]
-            with open(args.output, 'a') as output_file:
-                output_file.write(str(index_list[0]))
-                for i in range(len(index_list)-1):
-                    output_file.write(','+str(index_list[i+1]))
-                output_file.write('\n')
-            print(index_list)
-        else:
-            with open(args.output, 'a') as output_file:
-                output_file.write('0'+'\n')
-            print(index_list)
-        print("---------------------------------------------------------------")
+        with open(args.output, 'w') as output_file:
+            output_file.write(index_list_str)
+            
+            
